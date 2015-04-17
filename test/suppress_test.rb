509fd1f50f63ed12a33a946ff78f242ae9574d3d
Isaac <isaac.yonemoto@gmail.com>
2015-04-17 13:46:17 -0700

    added more tests to the suppress_test
    	modified:   test/suppress_test.rb
    	modified:   test/tables/suppresstest.basie

--- test/suppress_test.rb
+++ test/suppress_test.rb
@@ -33,19 +33,67 @@ class SuppressTest < Test::Unit::TestCase
     assert $BS.tables[:suppresstest].suppresslist == [:test2, :test3, :test4, :id]
   end
 
-  def test_suppression_output
+  def test_suppression_entire_table
     assert_equal [{:hash=>"zszwf8tWQsN3", :test=>"test 1"}], $BS.tables[:suppresstest].entire_table
   end
 
-  def test_suppress_output_restore_id
+  def test_suppress_entire_table_restore_id
     assert_equal [{:id => 1, :hash=>"zszwf8tWQsN3", :test=>"test 1"}], $BS.tables[:suppresstest].entire_table(:restore => [:id])
   end
 
-  def test_suppress_output_restore_tests
+  def test_suppress_entire_table_restore_tests
     assert_equal [{:hash=>"zszwf8tWQsN3",
                    :test=>"test 1", 
                    :test2 => "test 2",
                    :test3 => "test 3",
                    :test4 => "test 4"}], $BS.tables[:suppresstest].entire_table(:restore => [:test2, :test3, :test4])
+  end
+
+  def test_suppress_single_data
+    assert_equal ({:hash=>"zszwf8tWQsN3", :test=>"test 1"}), $BS.tables[:suppresstest].data_by_id('zszwf8tWQsN3')
+  end
+
+  def test_suppress_single_data_restore_id
+    assert_equal ({:id => 1, :hash=>"zszwf8tWQsN3", :test=>"test 1"}), $BS.tables[:suppresstest].data_by_id('zszwf8tWQsN3', :restore => [:id])
+  end
+
+  def test_suppress_single_data_restore_tests
+    assert_equal ({:hash=>"zszwf8tWQsN3",
+                   :test=>"test 1", 
+                   :test2 => "test 2",
+                   :test3 => "test 3",
+                   :test4 => "test 4"}), $BS.tables[:suppresstest].data_by_id('zszwf8tWQsN3', :restore => [:test2, :test3, :test4])
+  end
+
+  def test_suppress_query
+    assert_equal ({:hash=>"zszwf8tWQsN3", :test=>"test 1"}), $BS.tables[:suppresstest].data_by_query(:test, "test 1")
+  end
+
+  def test_suppress_query_restore_id
+    assert_equal ({:id => 1, :hash=>"zszwf8tWQsN3", :test=>"test 1"}), $BS.tables[:suppresstest].data_by_query(:test, "test 1", :restore => [:id])
+  end
+
+  def test_suppress_query_restore_tests
+    assert_equal ({:hash=>"zszwf8tWQsN3",
+                   :test=>"test 1", 
+                   :test2 => "test 2",
+                   :test3 => "test 3",
+                   :test4 => "test 4"}), $BS.tables[:suppresstest].data_by_query(:test, "test 1", :restore => [:test2, :test3, :test4])
+  end
+
+  def test_suppress_label
+    assert_equal ({:hash=>"zszwf8tWQsN3", :test=>"test 1"}), $BS.tables[:suppresstest].data_by_label("test 1")
+  end
+
+  def test_suppress_label_restore_id
+    assert_equal ({:id => 1, :hash=>"zszwf8tWQsN3", :test=>"test 1"}), $BS.tables[:suppresstest].data_by_label("test 1", :restore => [:id])
+  end
+
+  def test_suppress_label_restore_tests
+    assert_equal ({:hash=>"zszwf8tWQsN3",
+                   :test=>"test 1", 
+                   :test2 => "test 2",
+                   :test3 => "test 3",
+                   :test4 => "test 4"}), $BS.tables[:suppresstest].data_by_label("test 1", :restore => [:test2, :test3, :test4])
   end
 end
